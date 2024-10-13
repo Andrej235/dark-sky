2e2ff7c72d4baa1ec21e676be61d1921b54cdbef
Andrej Nenadić <andrejnana445@gmail.com>
2024-10-13 18:25:25 +0200

    Implement more stars using instances and add a title

--- src/ShootingStar.tsx
+++ src/ShootingStar.tsx
@@ -1,31 +1,53 @@
-import { Trail } from "@react-three/drei";
+import { Instance, Trail } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { Color, Mesh } from "three";
+import { Mesh } from "three";
 
-export default function ShootingStar() {
+type ShootingStarProps = {
+  particle: {
+    factor: number;
+    scale: number;
+    speed: number;
+    xFactor: number;
+    yFactor: number;
+    zFactor: number;
+  };
+};
+
+export default function ShootingStar({
+  particle: { scale, factor, speed, xFactor, yFactor, zFactor },
+}: ShootingStarProps) {
   const ref = useRef<Mesh>(null);
 
-  useFrame(({ clock }) => {
-    const t = clock.getElapsedTime() * 2;
-    ref.current?.position.set(
-      Math.sin(t) * 4,
-      Math.atan(t) * Math.cos(t / 2) * 2,
-      Math.cos(t) * 4
+  useFrame((state) => {
+    if (!ref.current) return;
+
+    const t = factor + state.clock.elapsedTime * (speed / 2);
+    ref.current.position.set(
+      Math.cos(t) +
+        Math.sin(t * 1) / 10 +
+        xFactor +
+        Math.cos((t / 10) * factor) +
+        (Math.sin(t * 1) * factor) / 10,
+      Math.sin(t) +
+        Math.cos(t * 2) / 10 +
+        yFactor +
+        Math.sin((t / 10) * factor) +
+        (Math.cos(t * 2) * factor) / 10,
+      Math.sin(t) +
+        Math.cos(t * 2) / 10 +
+        zFactor +
+        Math.cos((t / 10) * factor) +
+        (Math.sin(t * 3) * factor) / 4
     );
   });
-
   return (
     <Trail
+      children={<Instance ref={ref} scale={scale} />}
+      color={0xffffff}
+      attenuation={(t) => t * t}
       width={5}
       length={8}
-      color={new Color(2, 1, 10)}
-      attenuation={(t) => t * t}
-    >
-      <mesh ref={ref}>
-        <sphereGeometry args={[0.25]} />
-        <meshBasicMaterial color={[10, 1, 10]} toneMapped={false} />
-      </mesh>
-    </Trail>
+    />
   );
 }
--- src/StarrySky.tsx
+++ src/StarrySky.tsx
@@ -1,10 +1,25 @@
-import { OrbitControls, Stars } from "@react-three/drei";
+import { Html, Instances, OrbitControls, Stars } from "@react-three/drei";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import ShootingStar from "./ShootingStar";
+import { MathUtils } from "three";
+import Header from "./Header";
+
+const particles = Array.from({ length: 25 }, () => ({
+  scale: MathUtils.randFloat(0.5, 1),
+  factor: MathUtils.randInt(20, 100),
+  speed: MathUtils.randFloat(0.01, 0.75),
+  xFactor: MathUtils.randFloatSpread(40),
+  yFactor: MathUtils.randFloatSpread(10),
+  zFactor: MathUtils.randFloatSpread(10),
+}));
 
 export default function StarrySky() {
   return (
     <>
+      <Html>
+        <Header />
+      </Html>
+
       <color attach="background" args={["black"]} />
       <ambientLight intensity={1} />
 
@@ -15,7 +30,19 @@ export default function StarrySky() {
         <Bloom mipmapBlur luminanceThreshold={1} />
       </EffectComposer>
 
-      <ShootingStar />
+      <Instances
+        limit={particles.length}
+        castShadow
+        receiveShadow
+        position={[0, 2.5, 0]}
+      >
+        {particles.map((particle, i) => (
+          <ShootingStar key={i} particle={particle} />
+        ))}
+
+        <sphereGeometry args={[0.25]} />
+        <meshBasicMaterial color={[10, 1, 10]} toneMapped={false} />
+      </Instances>
     </>
   );
 }
